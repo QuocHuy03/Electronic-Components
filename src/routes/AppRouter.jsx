c8eba17991f070f830e604617ce0cb9af465351c
Lê Quốc Huy <97956803+QuocHuy03@users.noreply.github.com>
2023-10-25 13:21:58 +0700

    [UI] checkout

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,12 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import { URL_CONSTANTS } from "../constants/url.constants";
 import ProfilePage from "../pages/ProfilePage";
+import CartPage from "../pages/CartPage";
+import CheckoutPage from "../pages/CheckoutPage";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path={URL_CONSTANTS.HOME} element={<HomePage />} />
       <Route path={URL_CONSTANTS.PROFILE} element={<ProfilePage />} />
+      <Route path={URL_CONSTANTS.CART} element={<CartPage />} />
+      <Route path={URL_CONSTANTS.CHECKOUT} element={<CheckoutPage />} />
     </Routes>
   );
 }
