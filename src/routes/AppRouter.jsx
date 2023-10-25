4d5f0d777bde1d709623ce38fdb0e24127d859f8
NGUYEN TRUONG THINH <113654882+Thinhnt19@users.noreply.github.com>
2023-10-25 14:01:31 +0700

    [UI] update FilterPage

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import { URL_CONSTANTS } from "../constants/url.constants";
 import ProfilePage from "../pages/ProfilePage";
+import Filter from "../pages/FilterPage";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path={URL_CONSTANTS.HOME} element={<HomePage />} />
       <Route path={URL_CONSTANTS.PROFILE} element={<ProfilePage />} />
+      <Route path={URL_CONSTANTS.FILTER} element={<Filter />} />
     </Routes>
   );
 }
