70e1159a42955c38df678d36461afc8f4f34c442
Lê Quốc Huy <97956803+QuocHuy03@users.noreply.github.com>
2023-10-26 17:28:09 +0700

    Update AppRouter.jsx

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -15,9 +15,20 @@ import { URL_CONSTANTS } from "../constants/url.constants";
 import { useSelector } from "react-redux";
 
 const privateRoutes = [
-  { path: "/profile", component: ProfilePage },
-  { path: "/cart", component: CartPage },
-  { path: "/checkout", component: CheckoutPage },
+  { path: URL_CONSTANTS.PROFILE, component: ProfilePage },
+  { path: URL_CONSTANTS.CART, component: CartPage },
+  { path: URL_CONSTANTS.CHECKOUT, component: CheckoutPage },
+  { path: URL_CONSTANTS.NOTFOUND, component: NotFound },
+];
+const publicRoutes = [
+  { path: URL_CONSTANTS.HOME, component: HomePage },
+  { path: URL_CONSTANTS.LOGIN, component: LoginPage },
+  { path: URL_CONSTANTS.REGISTER, component: RegisterPage },
+  { path: URL_CONSTANTS.BLOG, component: BlogPage },
+  { path: URL_CONSTANTS.CONTACT, component: ContactPage },
+  { path: URL_CONSTANTS.BLOG_DETAIL, component: BlogDetail },
+  { path: URL_CONSTANTS.DETAIL, component: DetailProductPage },
+  { path: URL_CONSTANTS.NOTFOUND, component: NotFound },
 ];
 const AppRouter = () => {
   const auth = useSelector((state) => state.auth);
@@ -37,8 +48,13 @@ const AppRouter = () => {
             }
           />
         ))}
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
+      {publicRoutes.map((route) => (
+        <Route
+          path={route.path}
+          key={route.path}
+          element={<route.component />}
+        />
+      ))}
     </Routes>
   );
 };
