c72afb2ba3153f0ba2b133bdf2311cf58313853d
Đinh Tấn Trình <135788544+tantrinh29@users.noreply.github.com>
2023-11-17 18:47:25 +0700

    [UI]Update ProfilePage

--- src/pages/ProfilePage/index.jsx
+++ src/pages/ProfilePage/index.jsx
@@ -372,7 +372,7 @@ export default function ProfilePage() {
             <div className="update-logo w-full mb-9">
               <div className="flex xl:justify-center justify-start">
                 <div className="relative">
-                  <div className="sm:w-[188px] sm:h-[188px] w-[189px] h-[189px] rounded-full overflow-hidden relative" >
+                  <div className="sm:w-[188px] sm:h-[188px] w-[189px] h-[189px] rounded-full overflow-hidden relative">
                     <img
                       src={`https://ui-avatars.com/api/name=${user?.fullname}`}
                       alt
@@ -390,7 +390,7 @@ export default function ProfilePage() {
             className="w-[164px] h-[50px] bg-black text-white text-sm rounded-lg"
             style={{
               background: COLOR.BLUE,
-              color: "white"
+              color: "white",
             }}
           >
             Cập nhật thông tin
@@ -634,7 +634,9 @@ export default function ProfilePage() {
                               <path d="M8.8449 6.1337C7.15883 6.12476 5.787 4.74744 5.78906 3.06549C5.79113 1.37254 7.18564 -0.00685072 8.88753 2.55979e-05C10.5702 0.00690192 11.9413 1.38835 11.9392 3.07374C11.9372 4.77563 10.555 6.14264 8.8449 6.1337ZM9.94924 1.61802C9.38194 2.22039 8.85178 2.78287 8.31199 3.35636C8.11326 3.12806 7.94342 2.93278 7.76394 2.72786C7.48201 2.96853 7.22553 3.1872 6.95735 3.41618C7.28191 3.80125 7.57484 4.16708 7.8884 4.51502C8.10157 4.75225 8.43232 4.77082 8.65236 4.54458C9.33656 3.83907 10.0042 3.11775 10.6382 2.44593C10.4031 2.16332 10.1858 1.9027 9.94924 1.61802Z" />
                             </svg>
                           </span>
-                          <span className=" font-normal text-base">Quản lí đơn hàng</span>
+                          <span className=" font-normal text-base">
+                            Quản lí đơn hàng
+                          </span>
                         </div>
                       </div>
 
@@ -642,7 +644,7 @@ export default function ProfilePage() {
                         <div
                           onClick={() => handleTabClick(3)}
                           className={`flex space-x-3 items-center cursor-pointer ${
-                            activeTab === 4
+                            activeTab === 3
                               ? "text-blue-400"
                               : "text-slate-500 hover:text-black"
                           }`}
@@ -667,7 +669,43 @@ export default function ProfilePage() {
 
                       <div className="item group">
                         <div
-                          onClick={() => handleLogout(4)}
+                          onClick={() => handleTabClick(4)}
+                          className={`flex space-x-3 items-center cursor-pointer ${
+                            activeTab === 4
+                              ? "text-blue-400"
+                              : "text-slate-500 hover:text-black"
+                          }`}
+                        >
+                          <span>
+                            <svg
+                              width={14}
+                              height={20}
+                              viewBox="0 0 14 20"
+                              fill="none"
+                              className="fill-current"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path d="M11.864 15.982C11.4632 12.6503 9.25877 11.1115 7.04363 11.0543C5.70166 11.0221 4.5422 11.5123 3.57598 12.4607C2.60976 13.4054 2.19465 14.5935 1.99067 16.0285C1.37873 15.7458 0.809733 15.506 0.272945 15.2126C0.147695 15.1446 0.0582333 14.8941 0.0582333 14.7259C0.0439189 13.5629 -0.0849134 12.3819 0.0940158 11.2475C0.365988 9.54055 1.99067 8.23794 3.71555 8.05544C4.60661 7.96239 5.46547 7.95166 6.35296 8.16995C6.88617 8.30236 7.51243 8.19142 8.06711 8.05902C10.2357 7.52938 13.1559 8.64948 13.7571 10.997C14.0577 12.1672 14.0577 13.3661 13.8751 14.5613C13.8107 14.9979 13.6068 15.32 13.1737 15.481C12.7336 15.6385 12.3042 15.8102 11.864 15.982Z" />
+                              <path d="M10.6071 3.72194C10.5928 5.77962 8.96814 7.38999 6.93193 7.36494C4.8814 7.33989 3.24241 5.7009 3.26388 3.69331C3.28535 1.59984 4.90287 -0.0212607 6.94982 0.000210833C9.01824 0.0181038 10.6215 1.64994 10.6071 3.72194Z" />
+                              <path d="M10.9467 16.0178C10.9109 18.2795 9.07512 19.9972 6.74188 19.9507C4.58041 19.9077 2.77681 18.0719 2.80902 15.9498C2.8448 13.7454 4.69493 11.9776 6.9387 12.0062C9.17174 12.0384 10.9789 13.8492 10.9467 16.0178ZM6.84208 18.4834C7.27509 18.462 7.76893 18.4262 8.26278 18.419C8.62779 18.4154 8.74947 18.2222 8.74947 17.8966C8.74947 17.1808 8.75305 16.4687 8.74589 15.753C8.74589 15.6599 8.70652 15.5025 8.65642 15.4882C8.32719 15.4059 8.40234 15.1482 8.38087 14.9263C8.36298 14.7367 8.34151 14.547 8.30572 14.3645C8.15542 13.6309 7.64011 13.2122 6.91723 13.2229C6.22299 13.2337 5.64683 13.7418 5.52874 14.4432C5.4679 14.8154 5.65041 15.2735 5.17088 15.5096C5.14941 15.5204 5.14941 15.5919 5.14941 15.6349C5.14941 16.4508 5.1351 17.2667 5.16015 18.0826C5.16372 18.19 5.34981 18.3689 5.4679 18.3868C5.90449 18.4477 6.34108 18.4548 6.84208 18.4834Z" />
+                              <path
+                                d="M7.78818 15.3706C7.81323 14.8159 7.93491 14.2684 7.41601 13.9069C7.09036 13.6815 6.70745 13.6851 6.40327 13.9499C5.95953 14.3328 6.03826 14.8481 6.08836 15.3706C6.66451 15.3706 7.19414 15.3706 7.78818 15.3706ZM6.76113 17.643C6.8828 17.643 7.00448 17.643 7.11899 17.643C7.15836 17.3209 7.22635 17.0239 7.21561 16.7304C7.21204 16.6088 7.02953 16.4978 6.92933 16.3797C6.83986 16.4835 6.6824 16.5837 6.67883 16.6911C6.67167 16.9988 6.72892 17.3137 6.76113 17.643Z"
+                                fill="white"
+                              />
+                              <path d="M7.78818 15.3706C7.19414 15.3706 6.66451 15.3706 6.08836 15.3706C6.03826 14.8445 5.95953 14.3328 6.40327 13.9499C6.71103 13.6851 7.09394 13.6815 7.41601 13.9069C7.93491 14.2684 7.81323 14.8159 7.78818 15.3706Z" />
+                              <path d="M6.76261 17.6421C6.7304 17.3129 6.67314 17.0016 6.6803 16.6902C6.68388 16.5865 6.84134 16.4827 6.9308 16.3789C7.031 16.4934 7.21351 16.6079 7.21709 16.7296C7.22782 17.0231 7.15983 17.3201 7.12046 17.6421C7.00595 17.6421 6.88786 17.6421 6.76261 17.6421Z" />
+                            </svg>
+                          </span>
+
+                          <span className=" font-normal text-base">
+                            Quản lí địa chỉ
+                          </span>
+                        </div>
+                      </div>
+
+                      <div className="item group">
+                        <div
+                          onClick={() => handleLogout(5)}
                           className={`flex space-x-3 items-center cursor-pointer ${
                             activeTab === 5
                               ? "text-blue-400"
@@ -687,7 +725,9 @@ export default function ProfilePage() {
                               <path d="M13.4942 6.75005C13.4942 6.02423 13.5009 5.33425 13.492 4.64651C13.4875 4.27463 13.5927 3.96997 13.9532 3.81539C14.3136 3.66082 14.6046 3.79523 14.8688 4.06181C15.8002 5.0027 16.7405 5.93462 17.674 6.87326C18.1061 7.30786 18.1129 7.69094 17.6897 8.11882C16.7494 9.06642 15.8024 10.0073 14.8599 10.9549C14.6091 11.2058 14.327 11.3402 13.9755 11.1946C13.6129 11.0445 13.492 10.7533 13.4964 10.3769C13.5032 9.68695 13.4987 8.99473 13.4987 8.24875C13.3576 8.24875 13.2345 8.24875 13.1114 8.24875C12.2808 8.24875 11.4479 8.25099 10.6173 8.24651C10.0711 8.24427 9.75315 7.962 9.75987 7.4938C9.76435 7.03456 10.0912 6.75453 10.6352 6.75229C11.5666 6.75005 12.5024 6.75005 13.4942 6.75005Z" />
                             </svg>
                           </span>
-                          <span className=" font-normal text-base">Đăng xuất</span>
+                          <span className=" font-normal text-base">
+                            Đăng xuất
+                          </span>
                         </div>
                       </div>
                     </div>
@@ -707,9 +747,12 @@ export default function ProfilePage() {
                             </div>
                           </div>
                           <div className="quick-view-grid w-full flex justify-between items-center mt-3 ">
-                            <div className={`qv-item w-[252px] h-[208px] group  transition-all duration-300 ease-in-out p-6 rounded-lg`} style={{
-                              background: COLOR.BLUE
-                            }}>
+                            <div
+                              className={`qv-item w-[252px] h-[208px] group  transition-all duration-300 ease-in-out p-6 rounded-lg`}
+                              style={{
+                                background: COLOR.BLUE,
+                              }}
+                            >
                               <div className="w-[62px] h-[62px] rounded bg-white flex justify-center items-center">
                                 <span>
                                   <svg
@@ -747,9 +790,12 @@ export default function ProfilePage() {
                                 }
                               </span>
                             </div>
-                            <div className={`qv-item w-[252px] h-[208px] transition-all duration-300 ease-in-out p-6 rounded-lg`} style={{
-                              background: COLOR.BLUE
-                            }}>
+                            <div
+                              className={`qv-item w-[252px] h-[208px] transition-all duration-300 ease-in-out p-6 rounded-lg`}
+                              style={{
+                                background: COLOR.BLUE,
+                              }}
+                            >
                               <div className="w-[62px] h-[62px] rounded bg-white flex justify-center items-center">
                                 <span>
                                   <svg
@@ -781,9 +827,12 @@ export default function ProfilePage() {
                                 }
                               </span>
                             </div>
-                            <div className={`qv-item w-[252px] h-[208px] transition-all duration-300 ease-in-out p-6 rounded-lg`} style={{
-                              background: COLOR.BLUE
-                            }}>
+                            <div
+                              className={`qv-item w-[252px] h-[208px] transition-all duration-300 ease-in-out p-6 rounded-lg`}
+                              style={{
+                                background: COLOR.BLUE,
+                              }}
+                            >
                               <div className="w-[62px] h-[62px] rounded bg-white flex justify-center items-center">
                                 <span>
                                   <svg
@@ -809,7 +858,7 @@ export default function ProfilePage() {
                                 </span>
                               </div>
                               <p className="text-xl text-white group-hover:text-qblacktext mt-5">
-                                Đơn hàng đã giao 
+                                Đơn hàng đã giao
                               </p>
                               <span className="text-[40px] text-white group-hover:text-qblacktext font-bold leading-none mt-1 block">
                                 {
@@ -923,11 +972,10 @@ export default function ProfilePage() {
                                         className="flex w-full h-full items-center justify-center leading-0 opacity-1 rounded-lg"
                                         style={{
                                           background: COLOR.BLUE,
-                                          color: "white"
+                                          color: "white",
                                         }}
                                       >
-                                        <div className="w-full text-sm font-semibold"
-                                        >
+                                        <div className="w-full text-sm font-semibold">
                                           Đổi mật khẩu
                                         </div>
                                       </button>
@@ -1294,6 +1342,81 @@ export default function ProfilePage() {
                               </div>
                             </div>
                           </form>
+                        </React.Fragment>
+                      ) : activeTab === 4 ? (
+                        <React.Fragment>
+                          <div className="w-[80%]">
+                            <h5 className="text[16px]">Sổ địa chỉ</h5>
+                            <button className="inline-flex justify-center text-center w-full p-4 bg-white text-blue-500 mb-4 rounded-lg border border-dashed border-gray-300">
+                              <span size={26} className="inline-block w-26 h-26 bg-var(--button-icon-color, #757575) mask bg-center bg-no-repeat" />
+                              <div className="spacer css-1x3u27e" />
+                              Thêm địa chỉ mới
+                            </button>
+                            <div className="opacity-100 mt-3 p-4 rounded-lg bg-gray-50 relative">
+                              <div className="teko-card-body css-0">
+                                <div className="items-center justify-center">
+                                  <div
+                                    className="teko-col css-17ajfcv"
+                                    style={{ flex: "0 0 65%" }}
+                                  >
+                                    <div className="flex items-center gap-2">
+                                      <div
+                                        type="subtitle"
+                                        className="border-w-1 opacity-100 text-inherit font-medium text-base leading-6 "
+                                      >
+                                        trình
+                                      </div>
+                                      <span className="opacity-100 inline-flex items-center px-1/4 h-1rem rounded-1/4 bg-blue-100">
+                                        <div
+                                          type="caption"
+                                          color="primary600"
+                                          className="opacity-100 text-rgb-18-48-176 font-medium text-10 leading-16"
+                                        >
+                                          MẶC ĐỊNH
+                                        </div>
+                                      </span>
+                                    </div>
+                                    <div
+                                      type="body"
+                                      color="textSecondary"
+                                      className="mt-2 text-rgb-130-134-158 font-normal text-13"
+                                    >
+                                      Địa chỉ: da nang, Phường 17, Quận Gò Vấp,
+                                      Thành phố Hồ Chí Minh
+                                    </div>
+                                    <div
+                                      type="body"
+                                      color="textSecondary"
+                                      className="css-1npqwgp"
+                                    >
+                                      Điện thoại: 0987654321
+                                    </div>
+                                  </div>
+                                  <div
+                                    className="teko-col css-17ajfcv"
+                                    style={{ flex: "0 0 35%" }}
+                                  >
+                                    <div className=" justify-end">
+                                      <button
+                                        height="2rem"
+                                        className="opacity-100 h-2rem px-1.25rem rounded-0.25rem bg-transparent relative border border-1.5 border-red-500"
+                                        type="button"
+                                      >
+                                        <div
+                                          type="body"
+                                          color="error500"
+                                          className="text-[16px] text-red-500"
+                                        >
+                                          Chỉnh sửa
+                                        </div>
+                                        
+                                      </button>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
                         </React.Fragment>
                       ) : null}
                     </div>
