b56187e49d22938ae2041624ca563ecde048c68e
Lê Quốc Huy <97956803+QuocHuy03@users.noreply.github.com>
2023-11-17 22:10:09 +0700

    Update index.jsx

--- src/pages/CheckoutPage/index.jsx
+++ src/pages/CheckoutPage/index.jsx
@@ -927,7 +927,7 @@ export default function CheckoutPage() {
                                     type="submit"
                                     className=" text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
                                   >
-                                    Add
+                                    {isEditAddress ? "Update" : "Add"}
                                   </button>
                                   <button
                                     onClick={handleCloseModalAddress}
